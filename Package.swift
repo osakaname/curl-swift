8f5fa5b6fee8329a686d0008b97137c80911e39d
shikuma <las@osakaname.co.jp>
2025-03-05 16:16:23 +0900

    Upgrade swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "curl-swift",
     platforms: [
-        .macOS(.v10_10)
+        .macOS(.v10_15)
     ],
     products: [
         .library(
